3295acaa5f03d8eb925c8ef7a2c6d21a831aff9b
hoangpier <61455616+hoangpier@users.noreply.github.com>
2025-07-30 20:12:39 +0700

    Update play-event.py

--- play-event.py
+++ play-event.py
@@ -79,10 +79,10 @@ def run_event_bot_thread():
 
                     if 200 <= r.status_code < 300:
                         print(f"INFO: Click thành công! (Status: {r.status_code})")
-                        time.sleep(3)
+                        time.sleep(5)
                         return 
                     elif r.status_code == 429:
-                        retry_after = r.json().get("retry_after", 3)
+                        retry_after = r.json().get("retry_after", 5)
                         print(f"WARN: Bị rate limit! Sẽ thử lại sau {retry_after:.2f} giây...")
                         time.sleep(retry_after)
                     else:
